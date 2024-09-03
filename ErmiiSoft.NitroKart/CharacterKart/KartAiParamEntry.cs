5f2c85a646e373c2f950ea741369fa57b106c8a2
Ermelber <ermelber@live.it>
2024-09-03 08:47:59 +0200

    Add NitroKart project and KartAiParams classes

---
+++ ErmiiSoft.NitroKart/CharacterKart/KartAiParamEntry.cs
@@ -0,0 +1,8 @@
+﻿namespace ErmiiSoft.NitroKart.CharacterKart;
+
+public class KartAiParamEntry
+{
+    public uint RivalAggressiveness;
+    public uint GroupControl;
+    public uint CpuRubberBanding;
+}
---
+++ ErmiiSoft.NitroKart/CharacterKart/KartAiParams.cs
@@ -0,0 +1,20 @@
+﻿namespace ErmiiSoft.NitroKart.CharacterKart;
+
+public class KartAiParams
+{
+    public KartAiParamEntry GrandPrix50cc = new();
+    public KartAiParamEntry GrandPrix100cc = new();
+    public KartAiParamEntry GrandPrix150cc = new();
+
+    public KartAiParamEntry VersusEasy50cc = new();
+    public KartAiParamEntry VersusNormal50cc = new();
+    public KartAiParamEntry VersusHard50cc = new();
+
+    public KartAiParamEntry VersusEasy100cc = new();
+    public KartAiParamEntry VersusNormal100cc = new();
+    public KartAiParamEntry VersusHard100cc = new();
+
+    public KartAiParamEntry VersusEasy150cc = new();
+    public KartAiParamEntry VersusNormal150cc = new();
+    public KartAiParamEntry VersusHard150cc = new();
+}
